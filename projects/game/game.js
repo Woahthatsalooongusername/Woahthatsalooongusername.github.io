004ecaf7b2d946b7565ca8322d32055b0c81a328
Cristian Castro <cristian61238@gmail.com>
2026-03-09 11:11:29 -0400

    added some rooms and colors

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,5 +1,7 @@
-let gameActive = true; //this variable is required. 
-                       //to stop the game, set it to false.
+let gameActive = true;
+let haveLadder = false;
+let haveKey = false;
+let haveCheese = false;
 
 //Declare your other global variables here
 
@@ -8,48 +10,70 @@ let gameActive = true; //this variable is required.
 
 
 //Make one function for each location
-function locationA() {
-    clear();
-    print("\nYou are in location A!");
+function Bedroom() {
+    print("\nYou are in bedroom!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationB");
+        "\n\tHallway");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationb") {
-            locationB();
+        if (input.toLowerCase() === "hallway") {
+            Hallway();
         } else {
             stayHere();
-            waitThenCall(locationA);
+            waitThenCall(Bedroom);
         }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
-    clear();
-    print("\nYou are in location B!");
+function Hallway() {
+    print("\nYou are in Hallway!");
     print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+        "\n\tBedroom\n\tBathroom");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
+        if (input.toLowerCase() === "bedroom") {
+            Bedroom();
+	} else if (input.toLowerCase() === "bathroom") {
+		Bathroom();
         } else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(Hallway);
         }
+    }   
+    waitForInput(processInput);
+}
+
+function Bathroom() {
+
+	print("\nbathroom");
+	print("\nyou can go here:" +
+	    "\n\tHallway");
+
+function processInput(input){
+	if (input.toLowerCase() === "bathroom") {
+	    Bathroom();
+	} else {
+	    stayHere();
+	    waitThenCall(Bathroom);
+	}
     }
     waitForInput(processInput);
 }
+
+
+
+
+
 
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
-function start(){
-    print("Welcome to my game! Press any key to start");
+function start(){	
+    print("placeholder start");
 
-    function processInput(input){
-            locationA();
+    function processInput(input) {
+            Bedroom();
     }
     waitForInput(processInput);
 }
