ead1f1469b3d5e9f9f8e285b4e5d7d0b8841224f
Cristian Castro <cristian61238@gmail.com>
2026-03-17 07:49:41 -0400

    its all done

--- projects/game/game.js
+++ projects/game/game.js
@@ -8,7 +8,7 @@ function Outside() {
 clear();
 
 if (!haveSB) { 
-           print("\nloc: outside");
+           print("\nCurrent location: Outside");
            print("\nType the corresponding acronym to move:" +
                "\n\tFD ~ Frontdesk");
    
@@ -23,7 +23,10 @@ if (!haveSB) {
        waitForInput(processInput);
 }
 if (haveSB) {
-	print("YOU ESCAPED");
+	print("As you leave, You cast a firefall spell into the air");
+	print("A brilliant blue blaze flies out of the pages like a jet");
+	print("You cant wait to use this in another bank!");
+	print("THE END - THANK YOU FOR PLAYING!!!!");
 }
 }
 
@@ -32,8 +35,9 @@ if (haveSB) {
 //----------------------------------------------------
 function Frontdesk() {
 clear();
-    print("\nloc: frontdesk");
-    print("\nWhere do you want to go next? Say one of these choices:" +
+    print("\nCurrent location: Frontdesk");
+    print("\nThe vacant library glows slightly, almost as if it's reacting to your movement.");	
+    print("\nWhere do you want to go next?" +
         "\n\tO ~ Outside\n\tSR ~ Storageroom\n\tBS ~ Bookshelves");
 
     function processInput(input){
@@ -56,21 +60,22 @@ clear();
 
 if (!brokeLockpick && !haveMK) {
         print("\nYou try to open the storage room. it is locked.");
-        print("\nuse lockpick?");
+        print("\nWould you like to use your trusty lockpick?");
 	print("\n\ttype Yes or No");
 
 function processInput(input){ 
         if (input.toLowerCase() === "yes") {
 		brokeLockpick = "true";
-		print("it broke");
+		print("Before you can put it in the keyhole, it disintegrates in your hand.");
+		print("HOW???");
 		setTimeout(function() {
 		Frontdesk();
-		},3000);
+		},4000);
 	} else if (input.toLowerCase() === "no") {
 		print("You walk away to find another way in");
 		setTimeout(function() {
 		Frontdesk();
-		},3000);
+		},4000);
 	} else { 
 		stayHere();
 		waitThenCall(Storageroom);
@@ -81,7 +86,8 @@ waitForInput(processInput);
 }
 
 if (brokeLockpick && !haveMK) {
-	print("\nYou try to open the door again, but it's still locked.");
+	print("\nYou look at the pile of dust on the floor, formerly known as your lockpick");
+	print("\nMust be those new and fancy security spells");
 	print("\nPress enter to go back");
 
 function processInput(input){ 
@@ -92,7 +98,9 @@ waitForInput(processInput);
 
 if (!brokeLockpick && haveMK) {
 	print("\nYou use the master key and door unlocks");
-	print("\nput story here. also you can break seals now");
+	print("\nInside the Storage room, you see unused chairs and bookshelves collecting dust");
+	print("\nYou see a book laying on the ground labeled `A Beginners Guide To Breaking Seals!`");
+	print("\nYou quickly shove it into your bag");
 	print("\npress enter to leave");
 
 function processInput(input){
@@ -104,7 +112,13 @@ waitForInput(processInput);
 
 if (brokeLockpick && haveMK) { 
 	print("\nYou use the master key and door unlocks");
-	print("\nif only you found this before breaking your lockpick. also you can break seals now");
+	print("\nYou breath a sigh of relief that it didnt disintegrate like your lockpick did.");
+	print("\nInside the Storage room, you see unused chairs and bookshelves collecting dust");
+	print("\nYou see a book laying on the ground labeled `A Beginners Guide To Breaking Seals!`");
+	print("\nNow this makes up for the lockpick!");
+	print("\nYou quickly shove it into your bag");
+	print("\npress enter to leave");
+
 	print("\npress enter to leave");
 
 function processInput(input){ 
@@ -118,8 +132,11 @@ waitForInput(processInput);
 //---------------------------------------------------------
 function Bookshelves() {
 clear();
-	print("\nloc: bookshelves");
-	print("\nyou can go here:" +
+	print("\nCurrent location: Bookshelves");
+	print("\nYou briefly glance at the books as looking at every row in the magic library will probably take the entire night");
+	print("\nYou see titles such as `The Origin Of Ice Magic` - Written by Davi the Snowman, and `Necromancy: A Guide To Raising The Dead` - Written by The Skeleton King");
+	print("\nWhile these are interesting, They arent useful to you");
+	print("\nWhere do you want to go next?" +
 	    "\n\tFD ~ Frontdesk\n\tSOD ~ Staffonlydoor\n\tCL ~ Computerlab");
 
 function processInput(input){
@@ -140,16 +157,17 @@ function processInput(input){
  //-----------------------------------------------------
 function Computerlab() {
 clear();
-	print("\nloc computerlab");
+	print("\nCurrent location: Computer Lab");
 	print("\nYou walk inside and see guard fast asleep at a desk.");
 if (!haveMK) {
-	print("\nHanging by their waist, You see a fat-looking key chain with keys of varying colors on it. it is labeled `Master Key`");
+	print("\nHanging by their waist, You see a fat-looking key chain filled with keys of varying sizes on it. it is labeled `Master Key`");
 	print("\nYou carefully take it without waking the guard up");
 	print("\nPress enter to go back");
 }
 
 if (haveMK) {
 	print("\nThere isnt much to do here now that you have the master key");
+	print("\nThe guard is snoring loudly");
 	print("\nPress enter to go back");
 }
 function processInput(input) {
@@ -163,7 +181,7 @@ waitForInput(processInput);
 //-------------------------------------------------------
 function Staffonlydoor() {
 clear();
-	print("\nloc: staffonlydoor");
+	print("\nCurrent Location: Staff only door");
 	print("\nHere are your options:" +
 		"\n\tBS ~ Bookshelves\n\tHLO ~ Head librarian's office\n\tJC ~ Janitor's Closet");
 
@@ -200,10 +218,10 @@ waitForInput(processInput);
 
 if (canBreakSeals) {
 	haveSB = "true";
-	print("\nYou remember what you learned and recite the incantation");
-	print("\nIts a success! The seal breaks with a bubble poping sound");
-	print("\nYou got the spellbook!");
-	print("\nNow leave before anyone finds out");
+	print("\nYou remember the book you found in the storage room and take it out.");
+	print("\nyou recite an incantation and the seal breaks with a bubble poping sound");
+	print("\nInside, you see The Spellbook sitting in the librarian's desk! You take it with a smug look on your face.");
+	print("\nNow leave before anyone finds out!");
 	print("\npress enter to leave");
 
 	function processInput(input){
@@ -217,7 +235,10 @@ if (canBreakSeals) {
 function JanitorCloset() { 
 clear();
 
-	print("\nloc: Janitor's Closet");
+	print("\nCurrent location: Janitor's Closet");
+	print("\nYou would expect a giant, magic library like this one to have cooler cleaning equiptment");
+	print("\nBut no, Just a regular broom works apparently");
+	print("\nHow boring");
 	print("\npress enter to leave");
 function processInput(input) {
 	Staffonlydoor();
@@ -228,7 +249,11 @@ waitForInput(processInput);
 
  //-----------------------------------------------------
 function start(){	
-   print("placeholder start");
+   print("You arrive at the magic library at midnight. everyone has long gone home ");
+   print("For someone like you, this is the perfect time to strike!");
+   print("You glance around, making sure nobody is watching, and start picking the lock on the front door ");
+   print("You pull it off flawlessly!");
+   print("Press enter to continue");
 
     function processInput(input) {
             Outside();
